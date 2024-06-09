e0f522284db59929216a390b69df667386732589
a8dogdark <a8dogdark@gmail.com>
2024-06-09 19:59:20 -0400

    00

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/categorias', function () {
-    return view('front.categorias');
-});
-
-Route::get('/single', function () {
-    return view('front.single');
-});
-
-Route::get('/', function () {
-    return view('front.home');
-});
-
-
-/*
 Route::get('/', function () {
     return view('welcome');
 });
-*/
