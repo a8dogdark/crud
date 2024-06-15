b6a528ffd70f4b77cd9ac72c7f1a16969a86d762
a8dogdark <a8dogdark@gmail.com>
2024-06-15 19:54:14 -0400

    15-05-2024 19:54

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -3,5 +3,7 @@
 @section('titulo', 'Home')
 
 @section('content')
-    desde el home
+    desde el home <button type="button" class="btn btn-primary">Primary</button> <button type="button" class="btn btn-secondary">Secondary</button>
+    <p>este era mi boton que es nuevo para vite</p>
+    <p><h1>este es mi proyecto</h1></p>
 @endsection
