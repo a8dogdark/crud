6857a52522481022599561af3690277bbe95f6c0
a8dogdark <a8dogdark@gmail.com>
2024-06-16 15:24:07 -0400

    16-06-2024 15:23

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -6,4 +6,10 @@
     desde el home <button type="button" class="btn btn-primary">Primary</button> <button type="button" class="btn btn-secondary">Secondary</button>
     <p>este era mi boton que es nuevo para vite</p>
     <p><h1>este es mi proyecto</h1></p>
+    <div>
+        <i class="bi bi-person-circle"></i>
+    </div>
+    <div>
+        <i class="bi bi-person-vcard"></i>
+    </div>
 @endsection
