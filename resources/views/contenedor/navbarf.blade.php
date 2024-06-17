54e1bfdb0f9d236071095fa22a60046b5149b464
a8dogdark <a8dogdark@gmail.com>
2024-06-16 20:57:28 -0400

    16-06-2023

---
+++ resources/views/contenedor/navbarf.blade.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav m-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Blog</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Categorías
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+
+        <!-- esto no aparece en pantalla -->
+        <!--
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        -->
+
+        </ul>
+
+        <!--
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+    -->
+        <div class="d-flex">
+            <a href="#" title="Login de Usuario"><span><i class="bi bi-person-circle"></i></span></a> | <a href="#" title="Registro de Usuarios"><span><i class="bi bi-r-circle"></i></span></a>
+        </div>
+      </div>
+    </div>
+  </nav>
--- resources/views/layout/frontlayout.blade.php
+++ resources/views/layout/frontlayout.blade.php
@@ -8,6 +8,7 @@
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
+    @include('contenedor.navbarf')
     @yield('content')
 </body>
 </html>
